441e9415e62b9b761c4c016cd6c2541a76fade27
EMcdugald <edward.mcdugald@gmail.com>
2021-10-31 19:43:59 -0700

    more stuff

---
+++ 2021_10_01/misc/mymathradial.py
@@ -0,0 +1,68 @@
+
+
+import numpy as np
+
+#-----------------------------------------------------------------------
+# This function returns a radially symmetric function centered at (rcx,rcy)
+# with radiii rmax,rmin,   and its exactly computed laplacian.
+# These functions are computed on a grid (ndim x ndim) in the square
+# [-halfsize,halfsize] x [-halfsize,halfsize]
+#------------------------------------------------------------------------
+def radial(ndim,rcx,rcy,rmax,rmin,halfsize):
+   step = 2.0*halfsize/ndim
+
+   func   = np.zeros((ndim,ndim))
+   exlap  = np.zeros((ndim,ndim))
+
+   for i in range(0,ndim):
+      y = -halfsize + i*step
+      for j in range(0,ndim):
+         x = -halfsize + j*step
+         rad = np.sqrt((x-rcx)*(x-rcx)+(y-rcy)*(y-rcy))
+
+         if(rad > 1.e-30):
+
+           [hat,der,der2] = hatderivs(rmax,rmin,rad)
+           der = -der
+           der2 = -der2
+
+           rloga = np.log(rad)
+           exlapla = - (rloga*der2 + (2.0+rloga)*der/rad)
+           exlap[i,j] = exlapla
+           func[i,j] = -(1.-hat)*rloga
+
+   return [func,exlap]
+
+#----------------------------------------------------------------------
+# This function returns a hat function and its first and second derivatives
+# The hat function equals 1 on the interval [-rmin,rmin] and it vanishes
+# outside of the interval [-rmax,rmax]. Smooth C6 transition in between.
+#------------------------------------------------------------------------
+def hatderivs(rmax,rmin,xsign):
+    signum = 1.0
+    x = np.abs(xsign)
+    if(xsign < 0.0):
+       signum = -1.0
+
+    hat = 0.0
+    der = 0.0
+    der2 = 0.0
+
+    if(x > rmax):
+        return hat,der,der2
+
+    if(x < rmin):
+        hat = 1.0
+        return hat,der,der2
+
+    arg = (rmax-x)/(rmax-rmin)*np.pi
+    rkoef =  -1.0/(20.0*np.pi)
+
+    hat = rkoef*(  np.sin(6.0*arg)/3.0 - 3.0*np.sin(4.0*arg)  + 15.0*np.sin(2.0*arg) - 20.0*arg )
+
+    der = - signum*(np.sin(arg)**6) /(rmax-rmin)*3.20
+
+    der2 =6*(np.sin(arg)**5)*np.cos(arg) /(rmax-rmin)/(rmax-rmin)*3.20*np.pi
+
+    return [hat,der,der2]
+#-----------------------------------------------------------------
---
+++ 2021_10_01/misc/tryradial.py
@@ -0,0 +1,20 @@
+import utils
+import numpy as np
+import mymathradial as myrad
+
+rmax = 0.5
+rmin = 0.1
+
+rcx = 0.3
+rcy = -0.1
+
+ndim=2**9+1
+halfsize = 1.5
+
+print(ndim)
+
+[func,exlap] = myrad.radial(ndim,rcx,rcy,rmax,rmin,halfsize)
+
+
+utils.ndwrite(exlap,"pyth_ex_lap.d")
+utils.ndwrite(func,"pyth_rad_func.d")
